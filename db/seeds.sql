11c566e3a414caba3c1d0dbae7a8640a9cfd2b51
Dolan Pettit <dolanpettit@gmail.com>
2020-08-25 10:12:45 -0400

    Updating with seeds/sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
