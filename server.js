0d37e431e1072082e711495e21ea172fc86434c3
Dolan Pettit <dolanpettit@gmail.com>
2020-08-25 10:02:02 -0400

    Updating server.js with express and handlebars

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const app = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgers_controller");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.urlencoded({ extended: true })).use(express.json());
+
+app.use(express.static(__dirname + "/public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
