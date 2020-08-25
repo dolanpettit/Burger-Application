f96e662bf7be42a20cadc589c4127111c655cb01
Dolan Pettit <dolanpettit@gmail.com>
2020-08-25 10:34:13 -0400

    Updating with orm.js content

---
+++ config/orm.js
@@ -0,0 +1,30 @@
+const connection = require("./connection");
+
+var orm = {
+  all: function (tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput;
+    connection.query(queryString, function (err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  insertOne: function (tableInput, val1, cb) {
+    connection.query(
+      "INSERT INTO " + tableInput + "(burger_name) VALUES ('" + val1 + "');"
+    ),
+      function (err, result) {
+        if (err) throw err;
+      };
+    cb(result);
+  },
+  updateOne: function (tableInput, condition, cb) {
+    var queryString = "Update ?? SET devoured=true WHERE ?";
+    connection.query(queryString, [tableInput, condition], function (
+      err,
+      result
+    ) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+};
