5f536c0ddfdb4819ce11b7b50560f0fc3241f38e
Dolan Pettit <dolanpettit@gmail.com>
2020-08-25 09:51:14 -0400

    Updating with connection to database

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,20 @@
 const mysql = require("mysql");
-const pw = require()
+const pw = require("../pw");
 
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: pw,
+  database: "burger_db",
+});
 
+connection.connect(function (err) {
+  if (err) {
+    console.error("There was an error connecting: " + err.stack);
+    return;
+  }
+  console.log("Connected as ID " + connection.threadId);
+});
+
+module.exports = connection;
