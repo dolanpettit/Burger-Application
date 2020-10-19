30fca895c343632401284fc71779bd15a4c1553c
Dolan Pettit <dolanpettit@gmail.com>
2020-10-19 18:09:46 -0400

    Update: Added the heroku db

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,27 @@
 const mysql = require("mysql");
-const pw = require("../pw");
+// const pw = require("../pw");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: pw,
-  database: "burger_db",
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASSWORD,
+    database: "burger_db",
+  });
+}
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: process.env.DB_PASSWORD,
+//   database: "burger_db",
+// });
 
 connection.connect(function (err) {
   if (err) {
